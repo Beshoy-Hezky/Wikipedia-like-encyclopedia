69d841e779f419fd2b00eda123048c28ba6a165d
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-05-29 12:14:47 -0400

    F01: Add url of new page and update views for features 1 and 2

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,18 +1,32 @@
 import markdown2
 from django.shortcuts import render
-from markdown2 import Markdown
 from . import util
 
-def coverter(topic):
+
+def converter(topic):
     words = util.get_entry(topic)
     converter_obj = markdown2.Markdown()
     if words == None:
         return None
     else:
-        converter_obj.convert(words)
+        return converter_obj.convert(words)
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def content(request, topic):
+
+    words = util.get_entry(topic)
+    if words == None:
+        return render(request, "encyclopedia/error.html", {
+            "topic": topic
+        })
+    else:
+        return render(request, "encyclopedia/content.html",{
+            "topic": topic, "content": converter(topic)
+        })
 
