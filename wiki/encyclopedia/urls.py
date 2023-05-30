16e34623b4444ad9e3521882445f03609bf098d3
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-05-30 11:08:19 -0400

    F04,F05: Add URL specific for searching

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:topic>", views.content, name="content"),
-    path("random", views.random_page, name="random")
+    path("random/", views.random_page, name="random"),
+    path("search/", views.search_box, name="search_box"),
+    path("similarEntries/",views.search_box, name = "similar_entries")
 ]
