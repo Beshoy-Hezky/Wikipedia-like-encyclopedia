d9ac30bbef44bc0246185c650246bf8c1c3cf535
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-06-01 06:52:08 -0400

    F06: Add a new path in urls.py to create a new page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("wiki/<str:topic>", views.content, name="content"),
     path("random/", views.random_page, name="random"),
     path("search/", views.search_box, name="search_box"),
-    path("similarEntries/",views.search_box, name="similar_entries"),
-    path("newPage/",views.new_page, name="new_page")
+    path("similarEntries/", views.search_box, name="similar_entries"),
+    path("newPage/", views.new_page, name="new_page")
 ]
