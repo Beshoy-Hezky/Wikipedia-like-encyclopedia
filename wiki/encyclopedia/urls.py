0499fb86dca4f7babaf2942d33e2a6fbd76ddde1
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-06-01 10:56:13 -0400

    F07: Add 2 paths one for editing and one for submitting the edit

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("random/", views.random_page, name="random"),
     path("search/", views.search_box, name="search_box"),
     path("similarEntries/", views.search_box, name="similar_entries"),
-    path("newPage/", views.new_page, name="new_page")
+    path("newPage/", views.new_page, name="new_page"),
+    path("editPage/", views.edit_page, name="edit_page"),
+    path("submitEdit/", views.submit_edit, name="submit_edit")
 ]
